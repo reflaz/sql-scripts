17477c2a211112324101e6a637fe2befad8c985e
reflaz <refly.maliangkay@lazada.co.id>
2017-08-04 11:24:32 +0700

    update

--- shipment-charges-and-gain-loss-v3.0/04-05-grouping-calculate-other/config-weight-from-calculate/05b_scglv3_localdb_calculate_config_from_calculate_table_no_truncate.sql
+++ shipment-charges-and-gain-loss-v3.0/04-05-grouping-calculate-other/config-weight-from-calculate/05b_scglv3_localdb_calculate_config_from_calculate_table_no_truncate.sql
@@ -323,22 +323,8 @@ FROM
             aet.total_shipment_fee_mp_seller,
             aet.total_delivery_cost,
             aet.total_failed_delivery_cost,
-            ROUND(IFNULL(CASE
-                WHEN
-                    simple_weight > 0
-                        OR (simple_length * simple_width * simple_height) > 0
-                THEN
-                    simple_weight
-                ELSE config_weight
-            END, 0), 4) 'temp_weight_item',
-            ROUND(IFNULL(CASE
-                WHEN
-                    simple_weight > 0
-                        OR (simple_length * simple_width * simple_height) > 0
-                THEN
-                    (simple_length * simple_width * simple_height) / 6000
-                ELSE config_length * config_width * config_height / 6000
-            END, 0), 4) 'temp_volumetric_weight_item'
+			ROUND(IFNULL(config_weight, 0), 4) 'temp_weight_item',
+			ROUND(IFNULL(config_length * config_width * config_height / 6000, 0), 4) 'temp_volumetric_weight_item'
     FROM
         (SELECT 
         *
