d03dde20b2a132e0127033842eb5dfa51f62f6df
reflaz <refly.maliangkay@lazada.co.id>
2017-11-13 15:06:17 +0700

    update

--- daily-sales-summary/daily_sales_summary_by_date.sql
+++ daily-sales-summary/daily_sales_summary_by_date.sql
@@ -22,15 +22,31 @@ SET @extractend = '2017-03-05';-- This MUST be D + 1
 SELECT 
     order_date,
     business_unit,
+    lvl1,
     COUNT(DISTINCT order_nr) 'count_so',
     COUNT(bob_id_sales_order_item) 'count_soi',
-    SUM(unit_price) 'total_unit_price',
-    SUM(paid_price) 'total_paid_price',
-    SUM(shipping_amount) 'total_shipping_amount',
-    SUM(shipping_surcharge) 'total_shipping_surcharge',
-    SUM(coupon_money_value) 'total_coupon_money_value'
+    SUM(IFNULL(unit_price, 0)) 'total_unit_price',
+    SUM(IFNULL(paid_price, 0)) 'total_paid_price',
+    SUM(IFNULL(shipping_amount, 0)) 'total_shipping_amount',
+    SUM(IFNULL(shipping_surcharge, 0)) 'total_shipping_surcharge',
+    SUM(IFNULL(coupon_money_value, 0)) 'total_coupon_money_value',
+    SUM(IFNULL(nmv, 0)) 'nmv'
 FROM
     (SELECT 
+        so.order_date,
+            so.business_unit,
+            so.order_nr,
+            so.bob_id_sales_order_item,
+            so.unit_price,
+            so.paid_price,
+            so.shipping_amount,
+            so.shipping_surcharge,
+            so.coupon_money_value,
+            so.nmv,
+            catree.lvl1_id,
+            catree.lvl1
+    FROM
+        (SELECT 
         so.order_nr,
             soi.bob_id_sales_order_item,
             CASE
@@ -43,13 +59,71 @@ FROM
             soi.shipping_amount,
             soi.shipping_surcharge,
             soi.coupon_money_value,
-            DATE_FORMAT(so.created_at, '%Y-%m-%d') 'order_date'
+            DATE_FORMAT(so.created_at, '%Y-%m-%d') 'order_date',
+            cc.primary_category,
+            IFNULL(soi.paid_price / 1.1, 0) + IFNULL(soi.shipping_surcharge / 1.1, 0) + IFNULL(soi.shipping_amount / 1.1, 0) + IF(so.fk_voucher_type <> 3, IFNULL(soi.coupon_money_value / 1.1, 0), 0) 'nmv'
     FROM
         oms_live.ims_sales_order so
     LEFT JOIN oms_live.ims_sales_order_item soi ON so.id_sales_order = soi.fk_sales_order
     JOIN oms_live.ims_sales_order_item_status_history verified ON soi.id_sales_order_item = verified.fk_sales_order_item
         AND verified.fk_sales_order_item_status = 69
+    LEFT JOIN bob_live.catalog_simple cs ON soi.sku = cs.sku
+    LEFT JOIN bob_live.catalog_config cc ON cs.fk_catalog_config = cc.id_catalog_config
     WHERE
         so.created_at >= @extractstart
             AND so.created_at < @extractend) so
-GROUP BY order_date , business_unit
+    LEFT JOIN (SELECT 
+        IFNULL(lvl0_id, '') 'lvl0_id',
+            IFNULL(lvl0, '') 'lvl0',
+            IFNULL(lvl1_id, '') 'lvl1_id',
+            IFNULL(lvl1, '') 'lvl1',
+            IFNULL(lvl2_id, '') 'lvl2_id',
+            IFNULL(lvl2, '') 'lvl2',
+            IFNULL(lvl3_id, '') 'lvl3_id',
+            IFNULL(lvl3, '') 'lvl3',
+            IFNULL(lvl4_id, '') 'lvl4_id',
+            IFNULL(lvl4, '') 'lvl4',
+            IFNULL(lvl5_id, '') 'lvl5_id',
+            IFNULL(lvl5, '') 'lvl5',
+            IFNULL(lvl6_id, '') 'lvl6_id',
+            IFNULL(lvl6, '') 'lvl6',
+            cc0_id,
+            cc0_name
+    FROM
+        (SELECT 
+        MIN(IF(catree.lvl = 0, catree.cc1_id, NULL)) 'lvl0_id',
+            MIN(IF(catree.lvl = 0, catree.cc1_name, NULL)) 'lvl0',
+            MIN(IF(catree.lvl = 1, catree.cc1_id, NULL)) 'lvl1_id',
+            MIN(IF(catree.lvl = 1, catree.cc1_name, NULL)) 'lvl1',
+            MIN(IF(catree.lvl = 2, catree.cc1_id, NULL)) 'lvl2_id',
+            MIN(IF(catree.lvl = 2, catree.cc1_name, NULL)) 'lvl2',
+            MIN(IF(catree.lvl = 3, catree.cc1_id, NULL)) 'lvl3_id',
+            MIN(IF(catree.lvl = 3, catree.cc1_name, NULL)) 'lvl3',
+            MIN(IF(catree.lvl = 4, catree.cc1_id, NULL)) 'lvl4_id',
+            MIN(IF(catree.lvl = 4, catree.cc1_name, NULL)) 'lvl4',
+            MIN(IF(catree.lvl = 5, catree.cc1_id, NULL)) 'lvl5_id',
+            MIN(IF(catree.lvl = 5, catree.cc1_name, NULL)) 'lvl5',
+            MIN(IF(catree.lvl = 6, catree.cc1_id, NULL)) 'lvl6_id',
+            MIN(IF(catree.lvl = 6, catree.cc1_name, NULL)) 'lvl6',
+            cc0_id,
+            cc0_name
+    FROM
+        (SELECT 
+        *, IF(cc1_id = 1, @lvl:=0, @lvl:=@lvl + 1) 'lvl'
+    FROM
+        (SELECT 
+        cc0.id_catalog_category 'cc0_id',
+            cc0.name_en 'cc0_name',
+            cc1.id_catalog_category 'cc1_id',
+            cc1.name_en 'cc1_name',
+            cc1.lft,
+            cc1.rgt
+    FROM
+        bob_live.catalog_category cc0
+    LEFT JOIN bob_live.catalog_category cc1 ON cc0.lft >= cc1.lft
+        AND cc0.rgt <= cc1.rgt
+    ORDER BY cc0.id_catalog_category , cc1.lft , cc1.rgt
+    LIMIT 1000000000) catree) catree
+    GROUP BY catree.cc0_id) result) catree ON so.primary_category = catree.cc0_id
+    GROUP BY bob_id_sales_order_item) result
+GROUP BY order_date , business_unit , lvl1_id
