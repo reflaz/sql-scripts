3f5d1f7be131f38406006382fd4d24396ca564e1
unknown <Administrator@LAZ-ID-L-W-0638.lazada.intlaz>
2017-12-07 13:52:34 +0700

    update

---
+++ sc-transaction-details/account_statement_details_by_transaction_date.sql
@@ -0,0 +1,104 @@
+SET @extractstart = '2015-03-01';
+SET @extractend = '2015-04-01';
+
+SELECT 
+    *
+FROM
+    (SELECT 
+        tr.created_at 'Transaction Date',
+            tt.description 'Transaction Type',
+            tr.number 'Transaction Number',
+            tr.name 'Details',
+            tr.sku_seller 'Seller SKU',
+            tr.sku 'Lazada SKU',
+            tr.value 'Amount',
+            IFNULL(tr.taxes_vat, '') 'VAT in Amount',
+            IFNULL(tr.taxes_wht, '') 'WHT Amount',
+            IF(tr.is_wht_in_amount = 1, 'Yes', 'No') 'WHT included in Amount',
+            CONCAT_WS('-', DATE_FORMAT(ts.start_date, '%d %M %Y'), DATE_FORMAT(ts.end_date, '%d %M %Y')) 'Statement',
+            IF(ts.paid = 1, 'Paid', 'Not paid') 'Paid Status',
+            tr.order_nr 'Order No.',
+            tr.id_sales_order_item 'Order Item No.',
+            tr.src_status 'Order Item Status',
+            sp.name 'Shipping Provider',
+            tr.shipping_provider_type 'Shipping Speed',
+            st.name 'Shipment Type',
+            tr.id_sales_order_item 'Reference',
+            tr.description 'Comment',
+            '' AS 'PaymentRefId',
+            tr.number
+    FROM
+        (SELECT 
+        tr.*,
+            IFNULL(soi.name, soia.name) 'name',
+            IFNULL(soi.sku_seller, soia.sku_seller) 'sku_seller',
+            IFNULL(soi.sku, soia.sku) 'sku',
+            IFNULL(so.order_nr, soa.order_nr) 'order_nr',
+            IFNULL(soi.id_sales_order_item, soia.id_sales_order_item) 'id_sales_order_item',
+            IFNULL(soi.src_status, soia.src_status) 'src_status',
+            COALESCE(soi.shipping_provider_type, soia.shipping_provider_type, '') 'shipping_provider_type',
+            IFNULL(soi.fk_shipment_provider, soia.fk_shipment_provider) 'fk_shipment_provider',
+            IFNULL(soi.fk_shipment_type, soia.fk_shipment_type) 'fk_shipment_type'
+    FROM
+        asc_live.transaction tr
+    LEFT JOIN asc_live.sales_order_item soi ON tr.ref = soi.id_sales_order_item
+    LEFT JOIN asc_live.sales_order_item_archive soia ON tr.ref = soia.id_sales_order_item
+    LEFT JOIN asc_live.sales_order so ON IFNULL(soi.fk_sales_order, soia.fk_sales_order) = so.id_sales_order
+    LEFT JOIN asc_live.sales_order_archive soa ON IFNULL(soi.fk_sales_order, soia.fk_sales_order) = soa.id_sales_order
+    WHERE
+        tr.created_at >= @extractstart
+            AND tr.created_at < @extractend
+    GROUP BY tr.id_transaction) tr
+    LEFT JOIN asc_live.transaction_type tt ON tr.fk_transaction_type = tt.id_transaction_type
+    LEFT JOIN asc_live.transaction_statement ts ON tr.fk_transaction_statement = ts.id_transaction_statement
+    LEFT JOIN asc_live.shipment_provider sp ON tr.fk_shipment_provider = sp.id_shipment_provider
+    LEFT JOIN asc_live.shipment_type st ON tr.fk_shipment_type = st.id_shipment_type UNION ALL SELECT 
+        tr.created_at 'Transaction Date',
+            tt.description 'Transaction Type',
+            tr.number 'Transaction Number',
+            tr.name 'Details',
+            tr.sku_seller 'Seller SKU',
+            tr.sku 'Lazada SKU',
+            tr.value 'Amount',
+            IFNULL(tr.taxes_vat, '') 'VAT in Amount',
+            IFNULL(tr.taxes_wht, '') 'WHT Amount',
+            IF(tr.is_wht_in_amount = 1, 'Yes', 'No') 'WHT included in Amount',
+            CONCAT_WS('-', DATE_FORMAT(ts.start_date, '%d %M %Y'), DATE_FORMAT(ts.end_date, '%d %M %Y')) 'Statement',
+            IF(ts.paid = 1, 'Paid', 'Not paid') 'Paid Status',
+            tr.order_nr 'Order No.',
+            tr.id_sales_order_item 'Order Item No.',
+            tr.src_status 'Order Item Status',
+            sp.name 'Shipping Provider',
+            tr.shipping_provider_type 'Shipping Speed',
+            st.name 'Shipment Type',
+            tr.id_sales_order_item 'Reference',
+            tr.description 'Comment',
+            '' AS 'PaymentRefId',
+            tr.number
+    FROM
+        (SELECT 
+        tr.*,
+            IFNULL(soi.name, soia.name) 'name',
+            IFNULL(soi.sku_seller, soia.sku_seller) 'sku_seller',
+            IFNULL(soi.sku, soia.sku) 'sku',
+            IFNULL(so.order_nr, soa.order_nr) 'order_nr',
+            IFNULL(soi.id_sales_order_item, soia.id_sales_order_item) 'id_sales_order_item',
+            IFNULL(soi.src_status, soia.src_status) 'src_status',
+            COALESCE(soi.shipping_provider_type, soia.shipping_provider_type, '') 'shipping_provider_type',
+            IFNULL(soi.fk_shipment_provider, soia.fk_shipment_provider) 'fk_shipment_provider',
+            IFNULL(soi.fk_shipment_type, soia.fk_shipment_type) 'fk_shipment_type'
+    FROM
+        asc_live.transaction_archive tr
+    LEFT JOIN asc_live.sales_order_item soi ON tr.ref = soi.id_sales_order_item
+    LEFT JOIN asc_live.sales_order_item_archive soia ON tr.ref = soia.id_sales_order_item
+    LEFT JOIN asc_live.sales_order so ON IFNULL(soi.fk_sales_order, soia.fk_sales_order) = so.id_sales_order
+    LEFT JOIN asc_live.sales_order_archive soa ON IFNULL(soi.fk_sales_order, soia.fk_sales_order) = soa.id_sales_order
+    WHERE
+        tr.created_at >= @extractstart
+            AND tr.created_at < @extractend
+    GROUP BY tr.id_transaction) tr
+    LEFT JOIN asc_live.transaction_type tt ON tr.fk_transaction_type = tt.id_transaction_type
+    LEFT JOIN asc_live.transaction_statement ts ON tr.fk_transaction_statement = ts.id_transaction_statement
+    LEFT JOIN asc_live.shipment_provider sp ON tr.fk_shipment_provider = sp.id_shipment_provider
+    LEFT JOIN asc_live.shipment_type st ON tr.fk_shipment_type = st.id_shipment_type) result
+GROUP BY number;
--- voucher-usage/voucher_seller_usage_by_order_date.sql
+++ voucher-usage/voucher_seller_usage_by_order_date.sql
@@ -20,66 +20,88 @@ SET @extractstart = '2017-11-01';
 SET @extractend = '2017-12-01';-- This MUST be D + 1
 
 SELECT 
-    ascsel.id_seller 'asc_id_seller',
-    sup.id_supplier 'bob_id_supplier',
-    ascsel.short_code,
-    sup.name 'seller_name',
-    sup.type 'seller_type',
-    CASE
-        WHEN
-            soi.is_marketplace = 1
-                AND sa.fk_country = 101
-        THEN
-            'local'
-        WHEN ascsel.tax_class = 0 THEN 'local'
-        WHEN ascsel.tax_class = 1 THEN 'international'
-    END 'tax_class',
-    so.order_nr,
-    soi.bob_id_sales_order_item,
-    so.created_at 'order_date',
-    IFNULL((SELECT 
+    result.*,
+    (IFNULL((SELECT 
+                    SUM(IFNULL(tr.value, 0))
+                FROM
+                    asc_live.transaction tr
+                        LEFT JOIN
+                    asc_live.transaction_archive ta ON tr.number = ta.number
+                WHERE
+                    tr.ref = sc_id_sales_order_item
+                        AND tr.fk_transaction_type = 118
+                        AND ta.id_transaction IS NULL),
+            0) + IFNULL((SELECT 
+                    SUM(IFNULL(ta.value, 0))
+                FROM
+                    asc_live.transaction_archive ta
+                WHERE
+                    ta.ref = sc_id_sales_order_item
+                        AND ta.fk_transaction_type = 118),
+            0)) * - 1 'promotional_charges_vouchers'
+FROM
+    (SELECT 
+        ascsel.id_seller 'asc_id_seller',
+            sup.id_supplier 'bob_id_supplier',
+            COALESCE((SELECT 
+                    id_sales_order_item
+                FROM
+                    asc_live.sales_order_item
+                WHERE
+                    src_id = soi.id_sales_order_item), (SELECT 
+                    id_sales_order_item
+                FROM
+                    asc_live.sales_order_item_archive
+                WHERE
+                    src_id = soi.id_sales_order_item)) 'sc_id_sales_order_item',
+            ascsel.short_code,
+            sup.name 'seller_name',
+            sup.type 'seller_type',
+            CASE
+                WHEN
+                    soi.is_marketplace = 1
+                        AND sa.fk_country = 101
+                THEN
+                    'local'
+                WHEN ascsel.tax_class = 0 THEN 'local'
+                WHEN ascsel.tax_class = 1 THEN 'international'
+            END 'tax_class',
+            so.order_nr,
+            soi.bob_id_sales_order_item,
+            so.created_at 'order_date',
+            IFNULL((SELECT 
                     MIN(created_at)
                 FROM
                     oms_live.ims_sales_order_item_status_history
                 WHERE
                     fk_sales_order_item = soi.id_sales_order_item
-                        AND fk_sales_order_item_status = 27),
-            (SELECT 
+                        AND fk_sales_order_item_status = 27), (SELECT 
                     MIN(created_at)
                 FROM
                     oms_live.oms_package_status_history
                 WHERE
                     fk_package = pck.id_package
                         AND fk_package_status = 6)) 'delivered_date',
-    sois.name 'last_status',
-    soi.unit_price,
-    soi.paid_price,
-    soi.coupon_money_value,
-    so.coupon_code,
-    sovt.name 'coupon_type',
-    socr.cart_rule_name,
-    socr.cart_rule_discount
-FROM
-    oms_live.ims_sales_order so
-        LEFT JOIN
-    oms_live.ims_sales_order_item soi ON so.id_sales_order = soi.fk_sales_order
-        LEFT JOIN
-    oms_live.ims_sales_order_item_status sois ON soi.fk_sales_order_item_status = sois.id_sales_order_item_status
-        JOIN
-    oms_live.ims_sales_order_item_status_history verified ON soi.id_sales_order_item = verified.fk_sales_order_item
+            sois.name 'last_status',
+            soi.unit_price,
+            soi.paid_price,
+            soi.coupon_money_value,
+            so.coupon_code,
+            sovt.name 'coupon_type',
+            socr.cart_rule_name,
+            socr.cart_rule_discount
+    FROM
+        oms_live.ims_sales_order so
+    LEFT JOIN oms_live.ims_sales_order_item soi ON so.id_sales_order = soi.fk_sales_order
+    LEFT JOIN oms_live.ims_sales_order_item_status sois ON soi.fk_sales_order_item_status = sois.id_sales_order_item_status
+    JOIN oms_live.ims_sales_order_item_status_history verified ON soi.id_sales_order_item = verified.fk_sales_order_item
         AND verified.fk_sales_order_item_status = 67
-        LEFT JOIN
-    oms_live.ims_sales_order_voucher_type sovt ON so.fk_voucher_type = sovt.id_sales_order_voucher_type
-        LEFT JOIN
-    oms_live.ims_sales_order_cart_rule socr ON so.id_sales_order = socr.fk_sales_order
-        LEFT JOIN
-    oms_live.oms_package_item pi ON pi.fk_sales_order_item = soi.id_sales_order_item
-        LEFT JOIN
-    oms_live.oms_package pck ON pi.fk_package = pck.id_package
-        LEFT JOIN
-    bob_live.supplier sup ON sup.id_supplier = soi.bob_id_supplier
-        LEFT JOIN
-    bob_live.supplier_address sa ON sup.id_supplier = sa.fk_supplier
+    LEFT JOIN oms_live.ims_sales_order_voucher_type sovt ON so.fk_voucher_type = sovt.id_sales_order_voucher_type
+    LEFT JOIN oms_live.ims_sales_order_cart_rule socr ON so.id_sales_order = socr.fk_sales_order
+    LEFT JOIN oms_live.oms_package_item pi ON pi.fk_sales_order_item = soi.id_sales_order_item
+    LEFT JOIN oms_live.oms_package pck ON pi.fk_package = pck.id_package
+    LEFT JOIN bob_live.supplier sup ON sup.id_supplier = soi.bob_id_supplier
+    LEFT JOIN bob_live.supplier_address sa ON sup.id_supplier = sa.fk_supplier
         AND sa.id_supplier_address = (SELECT 
             MAX(id_supplier_address)
         FROM
@@ -88,21 +110,15 @@ FROM
             fk_supplier = sup.id_supplier
                 AND fk_country_region IS NOT NULL
                 AND address_type = 'warehouse')
-        LEFT JOIN
-    order_api.sales_order_item bsoi ON soi.bob_id_sales_order_item = bsoi.id_sales_order_item
-        LEFT JOIN
-    order_api.sales_order bso ON bso.id_sales_order = bsoi.fk_sales_order
-        LEFT JOIN
-    bob_live.sales_rule_apply sra ON bso.id_sales_order = sra.fk_sales_order
-        LEFT JOIN
-    bob_live.sales_rule sr ON sra.fk_sales_rule = sr.id_sales_rule
-        LEFT JOIN
-    bob_live.sales_rule_set srs ON sr.fk_sales_rule_set = srs.id_sales_rule_set
-        LEFT JOIN
-    asc_live.seller ascsel ON ascsel.src_id = sup.id_supplier
-WHERE
-    so.created_at > @extractstart
-        AND so.created_at <= @extractend
-        AND so.coupon_code IS NOT NULL
-        AND srs.seller_id IS NOT NULL
-        AND ascsel.id_seller IN ()
+    LEFT JOIN order_api.sales_order_item bsoi ON soi.bob_id_sales_order_item = bsoi.id_sales_order_item
+    LEFT JOIN order_api.sales_order bso ON bso.id_sales_order = bsoi.fk_sales_order
+    LEFT JOIN bob_live.sales_rule_apply sra ON bso.id_sales_order = sra.fk_sales_order
+    LEFT JOIN bob_live.sales_rule sr ON sra.fk_sales_rule = sr.id_sales_rule
+    LEFT JOIN bob_live.sales_rule_set srs ON sr.fk_sales_rule_set = srs.id_sales_rule_set
+    LEFT JOIN asc_live.seller ascsel ON ascsel.src_id = sup.id_supplier
+    WHERE
+        so.created_at > @extractstart
+            AND so.created_at <= @extractend
+            AND so.coupon_code IS NOT NULL
+            AND srs.seller_id IS NOT NULL
+            AND ascsel.id_seller IN ('582' , '2324', '2340', '6017', '24780', '27860', '31350', '33753', '34114', '34432', '37550', '38157', '42631', '59795', '68788', '70577', '73269', '75459', '78419', '81615', '81636', '82415', '82979', '100010151', '100011236', '100015812', '100042779')) result
